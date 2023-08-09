930c406959a0e58c02a743b0e376fcdc3efc3793
Juan Pedro Bastias <juanpedrobastiastorresi@gmail.com>
2023-08-09 08:31:21 -0300

    success franja horaria

--- components/FormularioTurno.js
+++ components/FormularioTurno.js
@@ -1,42 +1,97 @@
 import React, { useState, useEffect } from "react";
-import { format, parseISO, addMinutes } from "date-fns";
-import Link from "next/link";
 
 const Formulario = ({ zonasDepilar, fechasDisponibles }) => {
   const [selecciones, setSelecciones] = useState([]);
   const [sumaAcumulada, setSumaAcumulada] = useState({ precio: 0, tiempo: 0 });
   const [horariosDisponibles, setHorariosDisponibles] = useState([]);
+  const [tiempoAcumuladoMinutos, setTiempoAcumuladoMinutos] = useState(0);
 
   useEffect(() => {
-    // Filtrar las fechas disponibles en base al tiempo acumulado
-    const horarios = fechasDisponibles.filter((fecha) => {
-      const tiempoDisponible = calcularTiempoDisponible(
-        fecha.horaInicio,
-        fecha.horaFin
+    const updateHorarios = async () => {
+      const horarios = await filtrarHorariosDisponibles(
+        fechasDisponibles[0].fraccionamientoArray,
+        sumaAcumulada.tiempo
       );
-      return tiempoDisponible >= sumaAcumulada.tiempo;
-    });
-    setHorariosDisponibles(horarios);
-  }, [sumaAcumulada, fechasDisponibles]);
+      setHorariosDisponibles(horarios);
+    };
+
+    updateHorarios();
+  }, [sumaAcumulada.tiempo, fechasDisponibles]);
+
+  const filtrarHorariosDisponibles = (horarios, tiempoAcumulado) => {
+    const horariosFiltrados = [];
+    const [horaInicioPrimera, minutosInicioPrimera] =
+      horarios[0]?.split(":") || [];
+    const [horaFinUltima, minutosFinUltima] =
+      horarios[horarios.length - 1]?.split(":") || [];
+
+    // Convertimos la hora de inicio y fin en minutos para facilitar los cálculos
+    const minutosInicioTotal =
+      parseInt(horaInicioPrimera) * 60 + parseInt(minutosInicioPrimera);
+    const minutosFinTotal =
+      parseInt(horaFinUltima) * 60 + parseInt(minutosFinUltima);
+
+    // Si tiempoAcumulado es un número, lo convertimos a cadena
+    const tiempoAcumuladoStr =
+      typeof tiempoAcumulado === "number"
+        ? tiempoAcumulado.toString()
+        : tiempoAcumulado;
+
+    // Calculamos el tiempo acumulado necesario en minutos
+    const tiempoAcumuladoMinutos = parseInt(tiempoAcumuladoStr.split(" ")[0]);
+    console.log(tiempoAcumuladoMinutos);
+
+    // Creamos una función para determinar si una franja horaria es válida para el tiempo acumulado
+    const esFranjaValida = (inicio, fin) => {
+      return (
+        fin - inicio >= tiempoAcumuladoMinutos &&
+        inicio >= minutosInicioTotal &&
+        fin <= minutosFinTotal &&
+        fin <= 20 * 60 // 20:00 en minutos
+      );
+    };
+
+    let minutosInicio = minutosInicioTotal;
+    let minutosFin = minutosInicio + sumaAcumulada.tiempo;
+
+    while (minutosFin <= minutosFinTotal && minutosFin <= 20 * 60) {
+      if (esFranjaValida(minutosInicio, minutosFin)) {
+        const horaInicio = Math.floor(minutosInicio / 60);
+        const minutosInicioFraccion = minutosInicio % 60;
+        const horaFin = Math.floor(minutosFin / 60);
+        const minutosFinFraccion = minutosFin % 60;
+        const franjaHoraria = `${horaInicio
+          .toString()
+          .padStart(2, "0")}:${minutosInicioFraccion
+          .toString()
+          .padStart(2, "0")} a ${horaFin
+          .toString()
+          .padStart(2, "0")}:${minutosFinFraccion.toString().padStart(2, "0")}`;
+        horariosFiltrados.push(franjaHoraria);
+      }
+
+      // Avanzamos al siguiente intervalo de tiempo utilizando sumaAcumulada.tiempo
+      minutosInicio += sumaAcumulada.tiempo;
+      minutosFin = minutosInicio + sumaAcumulada.tiempo;
+
+      // Asegurémonos de no entrar en un bucle infinito si sumaAcumulada.tiempo es 0
+      if (sumaAcumulada.tiempo === 0) {
+        break;
+      }
+    }
+
+    return horariosFiltrados;
+  };
 
   const handleSeleccion = (e) => {
     const zonaSeleccionada = e.target.value;
     const [zona, precio, tiempo] = zonaSeleccionada.split(" | ");
 
-    // Verificar si es la segunda zona seleccionada
-    if (selecciones.length >= 1) {
-      // Restar 7 minutos al tiempo acumulado
-      setSumaAcumulada((prevSuma) => ({
-        precio: prevSuma.precio + parseInt(precio),
-        tiempo: prevSuma.tiempo + parseInt(tiempo) - 10,
-      }));
-    } else {
-      // Es la primera zona seleccionada, no restar tiempo
-      setSumaAcumulada((prevSuma) => ({
-        precio: prevSuma.precio + parseInt(precio),
-        tiempo: prevSuma.tiempo + parseInt(tiempo),
-      }));
-    }
+    // Actualizar tiempo acumulado
+    setSumaAcumulada((prevSuma) => ({
+      precio: prevSuma.precio + parseInt(precio),
+      tiempo: prevSuma.tiempo + parseInt(tiempo),
+    }));
 
     setSelecciones((prevSelecciones) => [...prevSelecciones, zonaSeleccionada]);
   };
@@ -44,30 +99,24 @@ const Formulario = ({ zonasDepilar, fechasDisponibles }) => {
   const eliminarSeleccion = (index) => {
     const zonaSeleccionada = selecciones[index];
     const [zona, precio, tiempo] = zonaSeleccionada.split(" | ");
+
+    // Actualizar tiempo acumulado
+    setSumaAcumulada((prevSuma) => ({
+      precio: prevSuma.precio - parseInt(precio),
+      tiempo: prevSuma.tiempo - parseInt(tiempo),
+    }));
+
     setSelecciones((prevSelecciones) =>
       prevSelecciones.filter((_, i) => i !== index)
     );
-
-    // Agregar 7 minutos al tiempo acumulado si hay más de una selección restante
-    if (selecciones.length > 1) {
-      setSumaAcumulada((prevSuma) => ({
-        precio: prevSuma.precio - parseInt(precio),
-        tiempo: prevSuma.tiempo - parseInt(tiempo) + 10,
-      }));
-    } else {
-      // Solo hay una selección restante, no agregar tiempo extra
-      setSumaAcumulada((prevSuma) => ({
-        precio: prevSuma.precio - parseInt(precio),
-        tiempo: prevSuma.tiempo - parseInt(tiempo),
-      }));
-    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const fechaDisponible = e.target.fechasDisponibles.value;
+
     const nombre = e.target.nombre.value;
     const apellido = e.target.apellido.value;
+    const fechaDisponible = e.target.fechasDisponibles.value;
     const zonasDepilar = selecciones;
 
     // Calcular precio y tiempo acumulado
@@ -116,14 +165,6 @@ const Formulario = ({ zonasDepilar, fechasDisponibles }) => {
     }
   };
 
-  // Función para calcular el tiempo disponible entre dos horas en minutos
-  const calcularTiempoDisponible = (horaInicio, horaFin) => {
-    const startTime = parseISO(`2023-01-01T${horaInicio}`);
-    const endTime = parseISO(`2023-01-01T${horaFin}`);
-    const tiempoDisponible = (endTime - startTime) / 1000 / 60;
-    return tiempoDisponible;
-  };
-
   return (
     <form onSubmit={handleSubmit} style={{ width: "600px" }}>
       <div>
@@ -138,14 +179,11 @@ const Formulario = ({ zonasDepilar, fechasDisponibles }) => {
 
       <div>
         <label htmlFor="fechasDisponibles">Fechas disponibles:</label>
-        <select id="fechasDisponiblesFecha" name="fechasDisponibles" required>
+        <select id="fechasDisponibles" name="fechasDisponibles" required>
           <option value="">Selecciona una fecha</option>
           {fechasDisponibles.map((fecha) => (
-            <option
-              key={fecha._id}
-              value={`${format(parseISO(fecha.dia), "yyyy-MM-dd")}`}
-            >
-              {`${format(parseISO(fecha.dia), "dd-MM-yyyy")}`}
+            <option key={fecha._id} value={`${formatDate(fecha.dia)}`}>
+              {`${formatDate(fecha.dia)}`}
             </option>
           ))}
         </select>
@@ -172,31 +210,19 @@ const Formulario = ({ zonasDepilar, fechasDisponibles }) => {
       </div>
 
       <div>
-        <label htmlFor="horariosDisponibles">
-          Horarios disponibles para las zonas seleccionadas:
-        </label>
+        <label htmlFor="horariosDisponibles">Horarios disponibles:</label>
         <select id="horariosDisponibles" name="horariosDisponibles" required>
           <option value="">Selecciona un horario</option>
-          {horariosDisponibles.map((fecha) => {
-            const horaInicioParts = fecha.horaInicio.split(":");
-            const horaFinParts = fecha.horaFin.split(":");
-            const horaInicioFormatted = `${horaInicioParts[0]}:${horaInicioParts[1]}`;
-            const horaFinFormatted = `${horaFinParts[0]}:${horaFinParts[1]}`;
-
-            return (
-              <option
-                key={fecha._id}
-                value={`${horaInicioFormatted} a ${horaFinFormatted}`}
-              >
-                {`${horaInicioFormatted} a ${horaFinFormatted}`}
-              </option>
-            );
-          })}
+          {horariosDisponibles.map((horario, index) => (
+            <option key={index} value={horario}>
+              {horario}
+            </option>
+          ))}
         </select>
       </div>
 
       <div>
-        <h3>Selecciones realizadas:</h3>
+        <h3>Zonas seleccionadas:</h3>
         <ul>
           {selecciones.map((seleccion, index) => (
             <li key={index}>
@@ -206,14 +232,22 @@ const Formulario = ({ zonasDepilar, fechasDisponibles }) => {
           ))}
         </ul>
       </div>
+
       <div>
-        <h3>Suma acumulada:</h3>
+        <h3>Totales</h3>
         <p>Precio total: ${sumaAcumulada.precio}</p>
         <p>Tiempo total: {sumaAcumulada.tiempo}''</p>
       </div>
+
       <button type="submit">Reservar</button>
     </form>
   );
 };
+
+// Función para formatear la fecha en "dd/mm/aaaa"
+function formatDate(dateString) {
+  const dateObj = new Date(dateString);
+  return dateObj.toLocaleDateString("es-ES");
+}
 
 export default Formulario;
